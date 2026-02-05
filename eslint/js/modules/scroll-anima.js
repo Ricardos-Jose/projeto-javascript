60848363e3494c5b9121f74cb2edfb41921d30a2
Ricardos-Jose <ricardojosegusmaodelima@gmail.com>
2026-02-05 14:39:54 -0300

    refeito o scroll

--- eslint/js/modules/scroll-anima.js
+++ eslint/js/modules/scroll-anima.js
@@ -2,24 +2,37 @@ export default class scrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.userHalfWindow = window.innerHeight * 0.6;
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.userHalfWindow < 0;
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.userHalfWindow),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
+  animaScroll() {}
+
   init() {
     if (this.sections.length) {
-      this.animaScroll();
-      window.addEventListener('scroll', this.animaScroll);
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
     }
+    return this;
   }
 }
