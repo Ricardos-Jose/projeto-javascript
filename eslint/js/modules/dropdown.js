de3cfbdd76842330ff8812c8457066eb930f30ab
Ricardos-Jose <ricardojosegusmaodelima@gmail.com>
2026-02-06 14:55:56 -0300

    refatorado dropdown, quase acerto.

--- eslint/js/modules/dropdown.js
+++ eslint/js/modules/dropdown.js
@@ -1,45 +1,50 @@
-import outsideClick from './cliqueFora.js';
-export default function initDropDown() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-  dropdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach((eventoUsuario) => {
-      menu.addEventListener(eventoUsuario, handleClick);
-    });
-  });
-
-  function handleClick(e) {
-    e.preventDefault();
-    this.classList.add('active');
-    outsideClick(this, ['click', 'touchstart'], () => {
-      this.classList.remove('active');
-    });
-  }
-}
-// TENTATIVA
 // import outsideClick from './cliqueFora.js';
-// export default class DropDown {
-//   constructor(menus) {
-//     this.dropdownMenus = document.querySelectorAll(menus);
-//     this.handleClick = this.handleClick.bind();
-//   }
-//   adicionarEventDrop() {
-//     this.dropdownMenus.forEach((menu) => {
-//       ['touchstart', 'click'].forEach((eventoUsuario) => {
-//         menu.addEventListener(eventoUsuario, this.handleClick);
-//       });
-//     });
-//   }
+// export default function initDropDown() {
+//   const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-//   handleClick(e) {
+//   dropdownMenus.forEach((menu) => {
+//     ['touchstart', 'click'].forEach((eventoUsuario) => {
+//       menu.addEventListener(eventoUsuario, handleClick);
+//     });
+//   });
+
+//   function handleClick(e) {
 //     e.preventDefault();
-//     e.classList.add('active');
+//     this.classList.add('active');
 //     outsideClick(this, ['click', 'touchstart'], () => {
 //       this.classList.remove('active');
 //     });
 //   }
-//   init() {
-//     this.handleClick();
-//     return this;
-//   }
 // }
+// TENTATIVA
+import outsideClick from './cliqueFora.js';
+export default class DropDown {
+  constructor(menus, events) {
+    this.dropdownMenus = document.querySelectorAll(menus);
+    this.handleClick = this.handleClick.bind();
+    this.activeClass = 'active';
+    if (events === undefined) this.events = ['touchstart', 'click'];
+    else this.events = this.events;
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.currentTarget.classList.remove(this.activeClass);
+    });
+  }
+  adicionarEventDrop() {
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((eventoUsuario) => {
+        menu.addEventListener(eventoUsuario, this.handleClick);
+      });
+    });
+  }
+  init() {
+    if (this.dropdownMenus.length) {
+      this.adicionarEventDrop();
+    }
+    return this;
+  }
+}
--- eslint/js/script.js
+++ eslint/js/script.js
@@ -4,19 +4,19 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/animacao-tabnav-menu.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
-import initDropDown from './modules/dropdown.js';
+import DropDown from './modules/dropdown.js';
 import initMobile from './modules/mobile-menu.js';
 import initDate from './modules/date-object.js';
 import fetchApi from './modules/fetchAnimais.js';
 import Bitcoin from './modules/bitcoin.js';
 
-initDropDown();
 initMobile();
 initDate();
 Bitcoin('https://blockchain.info/ticker', '.btc-preco');
 fetchApi('./animaisapi.json', '.numeros-grid');
 
-// const dropDown = new DropDown('[data-dropdown]');
+const dropDown = new DropDown('[data-dropdown]');
+dropDown.init();
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 const accordion = new Accordion('[data-accordion="accordion"] dt');
 accordion.initAccordion();
