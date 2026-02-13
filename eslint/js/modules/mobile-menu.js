024bc804c8613adb1cc1af075f2fa1255e875ffd
Ricardos-Jose <ricardojosegusmaodelima@gmail.com>
2026-02-13 12:30:41 -0300

    Correção

--- eslint/js/modules/mobile-menu.js
+++ eslint/js/modules/mobile-menu.js
@@ -10,6 +10,7 @@ export default class Mobile {
   }
 
   openMenu(event) {
+    event.preventDefault();
     this.menuButton.classList.add(this.activeClass);
     this.menuList.classList.add(this.activeClass);
     outsideClick(this.menuList, this.events, () => {
